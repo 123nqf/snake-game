291cc1d4d0cc6ae6f7c2763e63216f80b18aa347
123nqf <nafytabou2010@gmail.com>
2022-10-23 18:01:19 +0000

    my_second_commit

--- snake.py
+++ snake.py
@@ -1,12 +1,5 @@
 import pygame, sys, time, random
 
-
-
-# THANKS FOR WATHING, WHAT KIND OF GAMES U WANT TO SEE NEXT TIME? PLEASE STATE IT IN THE COMMENTS
-
-# CHEERSS
-
-
 speed = 15
 
 #windows sizes
